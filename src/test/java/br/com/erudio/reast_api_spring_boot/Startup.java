7d3f5dc25c438c3f06127a69dc06bc1915ebe859
rafaalmeida <rafaprof312@gmail.com>
2024-11-03 01:26:05 -0300

    initial commit

---
+++ src/test/java/br/com/erudio/reast_api_spring_boot/Startup.java
@@ -0,0 +1,13 @@
+package br.com.erudio.reast_api_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Startup {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
